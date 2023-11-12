d3595b48522eb81aa0479fe720f7b5fa74baecbb
Imagion <algernoncowperwood@gmail.com>
2023-11-12 23:20:57 +0300

    added layout

--- src/app.js
+++ src/app.js
@@ -1,5 +1,24 @@
 import './style.css'
 
 document.querySelector('#app').innerHTML = `
-  <h1 class="text-2xl">Header</h1>
+  <div
+  class="bg-neutral-700 p-8 rounded-3xl w-[min(100%,40rem)] [&>*+*]:mt-4"
+  >
+    <form class="flex">
+      <input
+        class="w-full p-2"
+        type="text"
+        name="newTodo"
+        id="newTodo"
+        placeholder="Что нужно сделать?"
+      />
+      <button class="bg-sky-700 py-2 px-4" type="submit">Добавить</button>
+    </form>
+
+    <ul class="[&>*:nth-child(odd)]:bg-neutral-500">
+      <li class="p-2">Далеко-далеко, за.</li>
+      <li class="p-2">Далеко-далеко, за.</li>
+      <li class="p-2">Далеко-далеко, за.</li>
+    </ul>
+  </div>
 `
